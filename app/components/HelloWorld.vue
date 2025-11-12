de650f8cccec6915cd0bf3dfecabce95c5dd60c3
Matyas Pesava <matyaspesava@gmail.com>
2025-11-12 10:14:31 +0100

    vytvoreni projektu

---
+++ app/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+    <h1>Hello, world!</h1>
+</template>
+
+<style scoped> 
+    h1 {
+        background-color: #000f00;
+    }
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const count = ref(0);
+const names = ["Eliška", "Tomáš", "Anna", "Petr"];
+</script>
+
+<template>
+  <HelloWorld/>
+  <HelloUser username="Matyas"/>
+
+  <HelloUser v-for="name in names" :key="name" :username="name" />
+
+  <HelloUser username="John Doe" />
+
+  <h2>Počítadlo: {{ count }}</h2>
+  <button @click="count+=100">+100</button>
+  <button @click="count+=10">+10</button>
+  <button @click="count++">+1</button>
+  <button @click="count--">-1</button>
+  <button @click="count-=10">-10</button>
+  <button @click="count-=100">-100</button>
+</template>
+
+<style scoped></style>
+
